9011ec1c3f391c00a8fb56dcb1446a118c72dd7a
shaelemay <146247343+shaelemay@users.noreply.github.com>
2025-04-24 10:33:55 -0700

    to do: line reveal

--- des342/proj1/script.js
+++ des342/proj1/script.js
@@ -2,26 +2,34 @@ gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline();
 // layers(#): background -> bird -> text (line 1, 2, 3, 4)
 
-gsap.set('.bird', {left:'40%', scale:0.6});
+gsap.set('.bird', {left:'40%', scale:0.6})
 gsap.set('.branch', {left:'40%', scale:0.8})
+gsap.set('.text', {sutoAlpha: 1, left: '10%', top: '50%'})
+
 
 ScrollTrigger.defaults({
   markers: true,
+  scrub: true,
+  snap: 0.1,
 })
 
 tl
   .from('.bird', {scale:0.4, ease:'none'})
-  .to('.bird', {ease:'none'})
-  .from('.branch', {ease:'none'})
+  .from('.branch', {left:'80%',ease:'none'})
 
 ScrollTrigger.create({
     id: "bird",
     trigger: ".bird",
-    start: "top 30%",
+    start: "top 10%",
     end: "bottom 50% +=500px",
     scrub:true,
-    animation: tl,
 });
+
+ScrollTrigger.create({
+    trigger: '.bird',
+    start: "top top",
+    end: "top top"
+})
 
 ScrollTrigger.create({
   id: "bird-sticky",
@@ -30,28 +38,13 @@ ScrollTrigger.create({
   end: "max",
   pin: true,
   pinSpacing: false,
+  animation: tl,
 })
 
 ScrollTrigger.create({
   id: "branch",
   trigger: ".branch",
-  start: "top 1300px",
-  end:"+=500px",
-  pin: true,
-  pinSpacing:false,
-  animation: tl,
+  start: "top 1000px",
+  end:"max",
+  scrub:true,
 })
-
-
-// ScrollTrigger.create({ 
-//   id: "bird-move",
-//   trigger: ".bird",
-//   start: "top 20%",
-//   end: "center 30%",
-//   scrub: true,
-
-// });
-
-// ScrollTrigger.create({
-
-// })
